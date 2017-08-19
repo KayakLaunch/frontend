89fdc9f0c96c870977fb1825ae60ccb09d784181
Chris Bingham <chris@pearit.co.uk>
2017-08-19 10:45:54 +0200

    Clean up post v2.0 release
    * Cleaned up and commented CSS file
    * Converted Google Maps JavaScript files to a generalised function for
    all route maps
    ** Modified route listing and route information page to use this
    generalised function
    * Removed unused JavaScript, KML, template, and font files
    * Cleaned up all HTML files
    * Commented all JavaScript files
    * Updated ReadMe and documentation files to cover v2.0
    * Updated license file

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-# kayaklaun.ch
-KayakLaun.ch is a pet project of mine I started to address a challenge from my hobby of sea kayaking. Finding places to launch and land a sea kayak with the required facilities nearby can be difficult. So, I created KayakLaun.ch to share a map I maintain of this information with fellow sea kayakers.
+# KayakLaun.ch
+KayakLaun.ch is a pet project of mine I started to address a challenge from my hobby of sea kayaking; finding places to launch and land a sea kayak with the required facilities nearby. So, I created KayakLaun.ch to share a map I maintain of this information and details of paddle routes with fellow sea kayakers.
 
-KayakLaun.ch is a responsive, Bootstrap-based single page website which makes heavy use of Google Maps APIs to enable visitors to browse and search a custom, multi-layered dataset.
+KayakLaun.ch is a responsive, [Bootstrap](https://getbootstrap.com)-based static website which makes heavy use of Google Maps APIs to enable visitors to browse and search custom, multi-layered datasets.
+
+## Dependencies
+The following APIs, frameworks, and resources were used in the creation of KayakLaun.ch:
+* [Bootstrap v3.3.7](https://getbootstrap.com): Creates the basic layout and structure of the pages 
+* [Google Maps JavaScript API](https://developers.google.com/maps/documentation/javascript/): Provides mapping capabilities
+* [Google Fonts: Cabin Sketch](https://fonts.google.com/specimen/Cabin+Sketch): Font for main brand text, headings, and calls to action
+* [Google Fonts: Comfortaa](https://fonts.google.com/specimen/Comfortaa): Font for main content and navigation bar
+* [Font Awesome v4.7.0](http://fontawesome.io): Used for social media icons
+* [Google Analytics](https://analytics.google.com): Provides site usage and audience analytics
--- js/googlemaps-launching.js
+++ js/googlemaps-launching.js
@@ -1,99 +1,102 @@
-(function($){
-  $(function(){
+// Function for displaying the launching spots map on the launching spots page
 
-      function initializeMap() {
-        var myLatLong = new google.maps.LatLng(0.0,0.0);
-        var mapOptions = {
-          center: myLatLong,
-          scrollwheel: true,
-          zoom: 3,
-          zoomControl: true,
-          zoomControlOptions: {
-              position: google.maps.ControlPosition.TOP_RIGHT
-          },
-          streetViewControl: false,
-          fullscreenControl: false,
-          mapTypeControl: true,
-          mapTypeControlOptions: {
-            style: google.maps.MapTypeControlStyle.DEFAULT,
-            position: google.maps.ControlPosition.TOP_RIGHT
-          },
-          mapTypeId: 'terrain'
+function initializeMap() {
 
-        };
-        var map = new google.maps.Map(document.getElementById("map-canvas"),
-            mapOptions);
+  // Set Google Maps options
+  // These specific to the launching spots page of the site
+  var myLatLong = new google.maps.LatLng(0.0,0.0);
+  var mapOptions = {
+    center: myLatLong,
+    scrollwheel: true,
+    zoom: 3,
+    zoomControl: true,
+    zoomControlOptions: {
+        position: google.maps.ControlPosition.TOP_RIGHT
+    },
+    streetViewControl: false,
+    fullscreenControl: false,
+    mapTypeControl: true,
+    mapTypeControlOptions: {
+      style: google.maps.MapTypeControlStyle.DEFAULT,
+      position: google.maps.ControlPosition.TOP_RIGHT
+    },
+    mapTypeId: 'terrain'
 
-        var mapData = 'https://github.com/binghamchris/kayaklaunch/blob/gh-pages/map/kayaklaunch-network.kmz?raw=true';
-        var kmlLayer = new google.maps.KmlLayer(mapData, {
-          suppressInfoWindows: false,
-          preserveViewport: false,
-          map: map
-        });
+  };
 
-        // Create the search box and link it to the UI element.
-        var input = document.getElementById('pac-input');
-        var searchBox = new google.maps.places.SearchBox(input);
-        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  // Bind the map to the targeted div
+  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
-        // Bias the SearchBox results towards current map's viewport.
-        map.addListener('bounds_changed', function() {
-          searchBox.setBounds(map.getBounds());
-        });
+  // Add the launching spots mapping data to the map
+  var mapData = 'https://github.com/binghamchris/kayaklaunch/blob/gh-pages/map/kayaklaunch-network.kmz?raw=true';
+  var kmlLayer = new google.maps.KmlLayer(mapData, {
+    suppressInfoWindows: false,
+    preserveViewport: false,
+    map: map
+  });
 
-        var markers = [];
-        // Listen for the event fired when the user selects a prediction and retrieve
-        // more details for that place.
-        searchBox.addListener('places_changed', function() {
-          var places = searchBox.getPlaces();
+  // Create the search box and link it to the UI element.
+  var input = document.getElementById('pac-input');
+  var searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-          if (places.length == 0) {
-            return;
-          }
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
 
-          // Clear out the old markers.
-          markers.forEach(function(marker) {
-            marker.setMap(null);
-          });
-          markers = [];
+  var markers = [];
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function() {
+    var places = searchBox.getPlaces();
 
-          // For each place, get the icon, name and location.
-          var bounds = new google.maps.LatLngBounds();
-          places.forEach(function(place) {
-            if (!place.geometry) {
-              console.log("Returned place contains no geometry");
-              return;
-            }
-            var icon = {
-              url: place.icon,
-              size: new google.maps.Size(71, 71),
-              origin: new google.maps.Point(0, 0),
-              anchor: new google.maps.Point(17, 34),
-              scaledSize: new google.maps.Size(25, 25)
-            };
+    if (places.length == 0) {
+      return;
+    }
 
-            // Create a marker for each place.
-            markers.push(new google.maps.Marker({
-              map: map,
-              icon: icon,
-              title: place.name,
-              position: place.geometry.location
-            }));
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+      marker.setMap(null);
+    });
+    markers = [];
 
-            if (place.geometry.viewport) {
-              // Only geocodes have viewport.
-              bounds.union(place.geometry.viewport);
-            } else {
-              bounds.extend(place.geometry.location);
-            }
-          });
-          map.fitBounds(bounds);
-        });
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      if (!place.geometry) {
+        console.log("Returned place contains no geometry");
+        return;
       }
-      function initialize() {
-        initializeMap();
-      }
-      google.maps.event.addDomListener(window, 'load', initializeMap);
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
 
-	}); // end of document ready
-})(jQuery); // end of jQuery name space
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
+}
+function initialize() {
+  initializeMap();
+}
+
+// Load the map when the page loads
+google.maps.event.addDomListener(window, 'load', initializeMap);
